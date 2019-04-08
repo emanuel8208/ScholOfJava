8ea92a62e259643ae28df402cac8a5f9d8a2bdbf
Emanuel <emanuel8208@gmail.com>
2019-04-08 19:58:47 +0300

    last

--- SchoolOfJava1/src/main/java/Main.java
+++ SchoolOfJava1/src/main/java/Main.java
@@ -1,2 +1,12 @@
-public class Main {
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import objects.Cat;
+import config.ProjectConfig;
+public class  Main {
+
+    public static void main (String Args []) {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class)){
+            Cat c = context.getBean(Cat.class);
+            System.out.println(c.name);
+        }
+    }
 }
